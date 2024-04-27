5cb50838567ce122c33b550eb1a47a8d7ffee831
Z3r0Tw0-ZT <sdhushanthan052@gmail.com>
2024-04-27 12:19:57 +0530

    api and front end

--- metalflawdetecta/settings.py
+++ metalflawdetecta/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'aimodel.apps.AimodelConfig',
+    'frontend.apps.FrontendConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
