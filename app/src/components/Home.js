f5bb149b25968ea5694bd8187cf5d1e34fdf2d0e
Dhushanthan <25567730+Z3r0Tw0-ZT@users.noreply.github.com>
2024-05-29 16:50:38 +0000

    React init

---
+++ app/src/components/Home.js
@@ -0,0 +1,11 @@
+function Home() {
+    return(
+        <div class="home">
+            <div class="intro">
+                <p>This is a simple deep learning project on identifying defects and metal surfaces. This deep learning model is trained on public dataset from kaggle.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+    return(
+        <div class="navbar">
+            <li>Home</li>
+            <li>Predict</li>
+            <li>About</li>
+        </div>
+    );
+}
+
+export default Navbar;
