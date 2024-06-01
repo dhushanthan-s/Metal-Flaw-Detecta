22708c595b1660c0512164b80d0ed4125e3977a4
Dhushanthan <25567730+Z3r0Tw0-ZT@users.noreply.github.com>
2024-06-01 20:10:09 +0530

    Revert "[Patch] Adding django backend (#4)" (#5)
    This reverts commit 2eb3026e36d20736682daa7463cc19892bebd08c.

---
+++ main.py
@@ -0,0 +1,62 @@
+from sklearn.datasets import load_files
+from sklearn.metrics import precision_score
+from keras import utils
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Loading dataset
+def load_dataset(path):
+    data = load_files(path)
+    files = np.array(data['filenames'])
+    targets = np.array(data['target'])
+    target_labels = np.array(data['target_names'])
+    
+    return files, targets, target_labels
+
+
+def convert_image_to_array(files):
+    images_as_array = []
+    for file in files:
+        # Convert to Numpy Array
+        images_as_array.append(img_to_array(load_img(file)))
+    return images_as_array
+
+
+if __name__ == '__main__':
+    valid_dir = 'NEU/valid'
+    model = load_model('defect_detection.keras')
+
+    x_test, y_test, target_labels = load_dataset(valid_dir)
+    no_of_classes = len(np.unique(y_test))
+    y_test = utils.to_categorical(y_test, no_of_classes)
+    x_test = np.array(convert_image_to_array(x_test))
+    print('Test set shape : ',x_test.shape)
+
+    x_test = x_test.astype('float32')/255
+
+    y_pred = model.predict(x_test)
+
+    # Let's visualize test prediction.
+    true_labels = np.argmax(y_test, axis=1)
+    predicted_labels = np.argmax(y_pred, axis=1)
+    precision = precision_score(true_labels, predicted_labels, average='weighted')
+    print("Precision = %.2f" %(precision * 100))
+
+    number_of_images = 16
+    rows = columns = 4
+
+    # plot a random sample of test images, their predicted labels, and ground truth
+    fig = plt.figure(figsize=(16, 9))
+    for i, idx in enumerate(np.random.choice(x_test.shape[0], size=number_of_images, replace=False)):
+        ax = fig.add_subplot(rows, columns, i + 1, xticks=[], yticks=[])
+        ax.imshow(np.squeeze(x_test[idx]))
+        pred_idx = np.argmax(y_pred[idx])
+        true_idx = np.argmax(y_test[idx])
+        ax.set_title("{} ({})".format(target_labels[pred_idx], target_labels[true_idx]),
+                     color=("green" if pred_idx == true_idx else "red"))
+
+    # Saving plot
+    plt.savefig('output.png')
